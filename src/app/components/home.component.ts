a90ae838b445e037ef8b27d148c10ae618d6774b
unknown <alba.alm.5@gmail.com>
2020-06-24 21:57:56 +0200

    home i footer

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'home',
-  templateUrl: '../views/home.html'
+  templateUrl: '../views/home.html',
+  styleUrls: ['../../assets/css/home.css']
 })
 export class homeComponent{
 
