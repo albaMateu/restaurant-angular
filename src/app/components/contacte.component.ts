e664e698a7389c65971849fa643499d51c752fe2
albaMateu <alba.alm.5@gmail.com>
2021-04-22 18:14:58 +0200

    horaris i reserva

--- src/app/components/contacte.component.ts
+++ src/app/components/contacte.component.ts
@@ -1,4 +1,4 @@
-import { GLOBAL } from './../services/global';
+import { GLOBAL, HORARI } from './../services/global';
 import { Component } from "@angular/core";
 
 
@@ -7,23 +7,31 @@ import { Component } from "@angular/core";
   templateUrl: '../views/contacte.html',
   styleUrls: ['../../assets/css/contacte.css']
 })
-export class contacteComponent{
-  public adreca:string;
-  public cp:number;
-  public poblacio:string;
-  public provincia:string;
+export class contacteComponent {
+  public adreca: string;
+  public cp: number;
+  public poblacio: string;
+  public provincia: string;
   public tel: string;
-  public email:string;
+  public email: string;
+  public i_m: string;
+  public f_m: string;
+  public i_v: string;
+  public f_v: string;
+  public semana_txt: string;
 
-  constructor(){
-    this.adreca= GLOBAL.adreca;
-    this.cp=GLOBAL.cp;
-    this.poblacio= GLOBAL.poblacio;
-    this.provincia= GLOBAL.provincia;
-    this.tel= GLOBAL.tel;
-    this.email=GLOBAL.email;
-
-    console.log("Contacte funciona");
+  constructor() {
+    this.adreca = GLOBAL.adreca;
+    this.cp = GLOBAL.cp;
+    this.poblacio = GLOBAL.poblacio;
+    this.provincia = GLOBAL.provincia;
+    this.tel = GLOBAL.tel;
+    this.email = GLOBAL.email;
+    this.i_m = HORARI.inici_m;
+    this.i_v = HORARI.inici_v;
+    this.f_m = HORARI.cierre_m;
+    this.f_v = HORARI.cierre_v;
+    this.semana_txt = HORARI.semana_txt;
 
   }
 
